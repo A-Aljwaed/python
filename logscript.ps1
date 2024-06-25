7023300667a4bb9863458e92993f777512f9ab84
ahmad aljwaed <ahmadaljawaed@gmail.com>
2024-06-25 20:59:00 +0200

    scripte hinzugefügt

---
+++ logscript.ps1
@@ -0,0 +1,19 @@
+﻿<#
+.SYNOPSIS
+Screibt eine Logdatei.
+.example
+beispiel >>>>   .\log.ps1 
+
+#>
+
+<# $logfilepath berechnet den logdatei pfad mit aktuellem Datum/Uhrzeit#>
+
+$logfilePath ="c:\temp\log_$(get-date -format "yyyy-MM-dd_HHmm").txt"
+function write-log(
+[parameter (Mandatory=$true)]
+[string]$logtxt,
+$loglevel){
+$logtxt= "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")-$logtxt"
+}
+
+write-log 
---
+++ volljaerigkeisPrüfer.ps1
@@ -0,0 +1,11 @@
+﻿function volljaehrig([string]$name,[parameter(Mandatory=$true)][int]$alter) {
+
+
+if($alter -gt 18){
+Write-Host "ok"
+Write-Host "$name du bist volljährig"
+}else {
+Write-Host "sorry $name  du bist nicht alt genug"}
+
+}
+volljaehrig
